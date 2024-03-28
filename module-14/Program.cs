706f56d2feaaa77197178638ecb128e3559784b4
youknovv <99376197+youknovv@users.noreply.github.com>
2024-03-29 03:19:25 +0700

    add 14.3.3

---
+++ module-14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        var phoneBook = new List<Contact>();
+        phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+        phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+        phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+        phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+        phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+        phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+        var sortedBook = from s in phoneBook orderby s.Name, s.LastName select s;
+
+        while (true)
+        {
+            var input = Console.ReadKey().KeyChar;
+            var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+            if (!parsed || pageNumber < 1 || pageNumber > 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Страницы не существует");
+            }
+            else
+            {
+                var pageContent = sortedBook.Skip((pageNumber - 1) * 2).Take(2);
+                Console.WriteLine();
+
+                foreach (var entry in pageContent)
+                    Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+public class Contact
+{
+    public Contact(string name, string lastName, long phoneNumber, String email)
+    {
+        Name = name;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public String Name { get; }
+    public String LastName { get; }
+    public long PhoneNumber { get; }
+    public String Email { get; }
+}
